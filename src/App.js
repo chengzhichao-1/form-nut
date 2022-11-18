6d153833ca3208e5444cfd0e4ff5836d5119102e
chengzhichao-1 <240087976@qq.com>
2022-11-18 15:44:46 +0800

    feat:引入formily

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MyRCFieldForm from "./pages/MyRCFieldForm";
+import FormilyPage from "./pages/FormilyPage";
 
 function App() {
   return (
     <div className="App">
-      <MyRCFieldForm />
+      {/* <MyRCFieldForm /> */}
+      <FormilyPage />
     </div>
   );
 }
